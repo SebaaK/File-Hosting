66febe13ade8af488ae05bce7b631e425797c163
SebaaK <sebaa.kot@gmail.com>
2022-07-05 14:02:41 +0200

    Next amendments in imports

--- src/main/java/kots/model/File.java
+++ src/main/java/kots/model/File.java
@@ -1,11 +1,24 @@
 package kots.model;
 
-import lombok.*;
-import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "files")
-@Data @Builder @NoArgsConstructor @AllArgsConstructor
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class File {
 
     @Id
